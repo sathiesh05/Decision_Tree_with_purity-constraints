59daab0f9297f3c6f83b4400083aefb634dc5306
SathieshPrabhu <148261347+sathiesh05@users.noreply.github.com>
2024-08-07 23:13:22 +0530

    Update Decision_tree_with_purity.py

--- Decision_tree_with_purity.py
+++ Decision_tree_with_purity.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_text,_tree
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-data = pd.read_csv('D:/ML/drug200_1722406166375.csv')
+data = pd.read_csv('drug200_1722406166375.csv')
 
 X = data[['Age', 'Sex', 'BP', 'Cholesterol']]
 y = data['Drug']
